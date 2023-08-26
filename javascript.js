25e93ac6cd2d27a3840a5507f891799426ba7550
ahzel <enciso.ac@gmail.com>
2023-08-26 10:39:35 +0800

    Fix invalid inputs

--- javascript.js
+++ javascript.js
@@ -13,18 +13,23 @@ function game() {
         return (prompt()).toLowerCase()
     }
     function playRound(playerSelection, computerSelection) {
-        if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
-            ++playerScore
-            console.log(`You Won! ${playerSelection} beats ${computerSelection}`)
-            console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
-        } else if (playerSelection === computerSelection) {
-            console.log("Tie")
-            console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
+        if (playerSelection === ("rock" || "paper" || "scissors")) {
+            if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
+                ++playerScore
+                console.log(`You Won! ${playerSelection} beats ${computerSelection}`)
+                console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
+            } else if (playerSelection === computerSelection) {
+                console.log("Tie")
+                console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
+            } else {
+                ++computerScore
+                console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
+                console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
+            }
         } else {
-            ++computerScore
-            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
-            console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
+            console.log("Invalid")
         }
+
     }
     let playerScore = 0
     let computerScore = 0
