307bb9032be84b2a1eae5e82ff78d8f990ff87ab
ahzel <enciso.ac@gmail.com>
2023-08-27 22:35:43 +0800

    Add UI

--- javascript.js
+++ javascript.js
@@ -1,43 +1,55 @@
-function game() {
-    function getComputerChoice() {
-        let choice = Math.floor(Math.random() * 3) + 1
-        if (choice === 1) {
-            return "rock"
-        } else if (choice ===2) {
-            return "paper"
-        } else {
-            return "scissors"
-        }
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3) + 1
+    if (choice === 1) {
+        return "Rock"
+    } else if (choice ===2) {
+        return "Paper"
+    } else {
+        return "Scissors"
     }
-    function getPlayerChoice() {
-        return (prompt()).toLowerCase()
-    }
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection === ("rock" || "paper" || "scissors")) {
-            if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
-                ++playerScore
-                console.log(`You Won! ${playerSelection} beats ${computerSelection}`)
-                console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
-            } else if (playerSelection === computerSelection) {
-                console.log("Tie")
-                console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
-            } else {
-                ++computerScore
-                console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
-                console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
-            }
-        } else {
-            console.log("Invalid")
-        }
-
-    }
-    let playerScore = 0
-    let computerScore = 0
-    playRound(getPlayerChoice(),getComputerChoice())
-    playRound(getPlayerChoice(),getComputerChoice())
-    playRound(getPlayerChoice(),getComputerChoice())
-    playRound(getPlayerChoice(),getComputerChoice())
-    playRound(getPlayerChoice(),getComputerChoice())
 }
 
-game()
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")) {
+        ++playerScore
+        scoreDiv.textContent = `Player: ${playerScore}, Computer: ${computerScore}`
+        resultDiv.textContent = `You Won! ${playerSelection} beats ${computerSelection}`
+    } else if (playerSelection === computerSelection) {
+        scoreDiv.textContent = `Player: ${playerScore}, Computer: ${computerScore}`
+        resultDiv.textContent = "Tie"
+    } else {
+        ++computerScore
+        scoreDiv.textContent = `Player: ${playerScore}, Computer: ${computerScore}`
+        resultDiv.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+    if (playerScore === 5 || computerScore === 5) {
+        playerScore = 0
+        computerScore = 0
+        scoreDiv.textContent = `Player: ${playerScore}, Computer: ${computerScore}`
+        if (playerScore === 5) {
+            resultDiv.textContent = "You Win!"
+        } else {
+            resultDiv.textContent = "You Lost!"
+        }
+    }
+}
+
+let playerScore = 0
+let computerScore = 0
+
+let scoreDiv = document.querySelector(".score")
+let resultDiv = document.querySelector(".result")
+
+let rockButton = document.querySelector(".rock")
+let paperButton = document.querySelector(".paper")
+let scissorsButton = document.querySelector(".scissors")
+
+rockButton.addEventListener("click", () => {
+    playRound("Rock",getComputerChoice())
+})
+paperButton.addEventListener("click", () => {
+    playRound("Paper",getComputerChoice())
+})
+scissorsButton.addEventListener("click", () => {
+    playRound("Scissors",getComputerChoice())
+})
